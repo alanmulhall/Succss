a189904dc66d5df2f4cfecc6be649cf6a69267df
alan mulhall <alanmulhall@hotmail.com>
2015-04-02 15:50:53 -0700

    Patch function.bind, so we can use React.js

--- succss.js
+++ succss.js
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  * Succss module.
  *
  * The MIT License (MIT)
@@ -33,6 +33,47 @@ function Succss() {
 
     var casperInstance = self.casper;
 
+    casperInstance.on("page.initialized", function () {
+      this.evaluate(function () {
+          var isFunction = function (o) {
+              return typeof o === "function";
+          };
+
+          var bind,
+              slice = [].slice,
+              proto = Function.prototype,
+              featureMap;
+
+          featureMap = {
+              "function-bind": "bind"
+          };
+
+          function has(feature) {
+              var prop = featureMap[feature];
+              return isFunction(proto[prop]);
+          }
+
+          // check for missing features
+          if (!has("function-bind")) {
+              // adapted from Mozilla Developer Network example at
+              // https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Function/bind
+              bind = function bind(obj) {
+                  var args = slice.call(arguments, 1),
+                      self = this,
+                      nop = function () {
+                      },
+                      bound = function () {
+                          return self.apply(this instanceof nop ? this : (obj || {}), args.concat(slice.call(arguments)));
+                      };
+                  nop.prototype = this.prototype || {}; // Firefox cries sometimes if prototype is undefined
+                  bound.prototype = new nop();
+                  return bound;
+              };
+              proto.bind = bind;
+          }
+      });
+    });
+
     var fs = self.fs;
     var options = self.allOptions;
     var utils = self.utils;
@@ -61,7 +102,7 @@ function Succss() {
     var capturesFound = false;
     var captureFilters = false;
 
-    var viewportsData = self.viewports || { 
+    var viewportsData = self.viewports || {
       'default': {
         'width':1366,
         'height':768
